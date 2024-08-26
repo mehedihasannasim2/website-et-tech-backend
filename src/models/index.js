3419a3ecbe959ab2747bb16836ce7cacd41c52ef
nasim-etlimited <nasim@etlimited.net>
2024-08-26 15:38:09 +0600

    pulling neaz branch

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
+module.exports.Job = require('./Job.model');
 module.exports.Blog = require('./blog.model');
-module.exports.Job = require('./job.model');
 module.exports.JobApplicant = require('./jobApplicant.model')
